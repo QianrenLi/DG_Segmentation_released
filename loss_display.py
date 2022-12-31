a01b556732c8d3d7e648b61c30e0797d9767415f
QianrenLi <908559578@qq.com>
2022-12-31 19:03:47 +0800

    path change

--- loss_display.py
+++ loss_display.py
@@ -16,23 +16,23 @@ def loss_read(log_file_name):
 epoch_number = 20
 
 # ################### Log File Read ########################
-# log_file_name = ('./no_DG_epoch_DICE_%d.txt')%(epoch_number)
-# log_file_name = ('./DG_epoch_%d.txt')%(epoch_number)
-# log_file_name = ('./Data_arg_no_DG_DICE_epoch_%d.txt')%(epoch_number)
-log_file_name = ('./Data_arg_no_DG_epoch_%d.txt') % (epoch_number)
-# log_file_name = ('./Data_DG_D1_epoch_%d.txt')%(epoch_number)
-# log_file_name = ('./Data_arg_DG_epoch_s03_r1_t%d.txt')%(epoch_number)
-# log_file_name = ('./Data_arg_DG_D1_epoch_%d.txt')%(epoch_number)
-# log_file_name = ('./Data_arg_DG_D2_epoch_%d.txt')%(epoch_number)
-# log_file_name = ('./Data_arg_DG_D3_epoch_%d.txt')%(epoch_number)
-# log_file_name = ('./Data_DG_D1_epoch_%d.txt')%(epoch_number)
+# log_file_name = ('./results/Loss/no_DG_epoch_DICE_%d.txt')%(epoch_number)
+# log_file_name = ('./results/Loss/DG_epoch_%d.txt')%(epoch_number)
+# log_file_name = ('./results/Loss/Data_arg_no_DG_DICE_epoch_%d.txt')%(epoch_number)
+log_file_name = ('./results/Loss/Data_arg_no_DG_epoch_%d.txt') % (epoch_number)
+# log_file_name = ('./results/Loss/Data_DG_D1_epoch_%d.txt')%(epoch_number)
+# log_file_name = ('./results/Loss/Data_arg_DG_epoch_s03_r1_t%d.txt')%(epoch_number)
+# log_file_name = ('./results/Loss/Data_arg_DG_D1_epoch_%d.txt')%(epoch_number)
+# log_file_name = ('./results/Loss/Data_arg_DG_D2_epoch_%d.txt')%(epoch_number)
+# log_file_name = ('./results/Loss/Data_arg_DG_D3_epoch_%d.txt')%(epoch_number)
+# log_file_name = ('./results/Loss/Data_DG_D1_epoch_%d.txt')%(epoch_number)
 # ################### Log File Read ########################
 
 loss_DICE = loss_read(log_file_name)
-no_DG_epoch = ('./no_DG_epoch_DICEBCE_%d.txt')%(epoch_number)
+no_DG_epoch = ('./results/Loss/no_DG_epoch_DICEBCE_%d.txt')%(epoch_number)
 loss_DICE_CE = loss_read(no_DG_epoch)
 
-no_DG_cross_entropy = ('./no_DG_epoch_cross_entropy_%d.txt')%(epoch_number)
+no_DG_cross_entropy = ('./results/Loss/no_DG_epoch_cross_entropy_%d.txt')%(epoch_number)
 loss_CE = loss_read(no_DG_cross_entropy)
 
 x_train_loss = np.array(range(400))
@@ -45,13 +45,13 @@ plt.ylabel('Loss',fontsize='large',fontweight='heavy')
 # plt.plot(x_train_loss/20, loss_DICE_CE, linewidth=1, linestyle="solid", label="Without Data Augment")
 # #################### Compare Data Augment #####################
 
-# PATH = ('./no_DG_epoch_DICEBCE_%d.txt')%(epoch_number)
+# PATH = ('./results/Loss/no_DG_epoch_DICEBCE_%d.txt')%(epoch_number)
 # loss1 = loss_read(PATH)
-# PATH = ('./DG_epoch_%d.txt') % (epoch_number)
+# PATH = ('./results/Loss/DG_epoch_%d.txt') % (epoch_number)
 # loss2 = loss_read(PATH)
-# PATH = ('./Data_arg_no_DG_epoch_%d.txt') % (epoch_number)
+# PATH = ('./results/Loss/Data_arg_no_DG_epoch_%d.txt') % (epoch_number)
 # loss1 = loss_read(PATH)
-# PATH = ('./Data_arg_DG_epoch_s03_r05_t%d.txt') % (epoch_number)
+# PATH = ('./results/Loss/Data_arg_DG_epoch_s03_r05_t%d.txt') % (epoch_number)
 # loss2 = loss_read(PATH)
 
 
@@ -60,9 +60,9 @@ plt.ylabel('Loss',fontsize='large',fontweight='heavy')
 # plt.plot(x_train_loss/20, loss2, linewidth=1, linestyle="solid", label="DG")
 # #################### Compare Domain Generalization #####################
 
-PATH = ('./no_DG_epoch_DICE_%d.txt')%(epoch_number)
+PATH = ('./results/Loss/no_DG_epoch_DICE_%d.txt')%(epoch_number)
 loss2 = loss_read(PATH)
-PATH = ('./no_DG_epoch_DICE_t%d.txt') % (epoch_number)
+PATH = ('./results/Loss/no_DG_epoch_DICE_t%d.txt') % (epoch_number)
 loss1 = loss_read(PATH)
 
 #################### Compare DICE implementation #####################
@@ -76,7 +76,7 @@ plt.plot(x_train_loss/20, loss2, linewidth=1, linestyle="solid", label="DICE 2 c
 # plt.plot(x_train_loss/20, loss_CE, linewidth=1, linestyle="solid", label="Cross Entropy Loss")
 #################### Compare Loss Function ####################
 
-Arg_DICE = ('./Data_arg_no_DG_DICE_epoch_%d.txt')%(epoch_number)
+Arg_DICE = ('./results/Loss/Data_arg_no_DG_DICE_epoch_%d.txt')%(epoch_number)
 loss_DICE_arg = loss_read(Arg_DICE)
 # plt.plot(x_train_loss/20, loss_DICE_arg/2, linewidth=1, linestyle="solid", label="DICE + Data Augment")
 ax = plt.gca()  # 获取当前图像的坐标轴信息
